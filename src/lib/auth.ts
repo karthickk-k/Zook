8eaf1524d945f9a4e849ca78a5b5992fa0aa5aad
karthickk-k <karthick.k@mitrahsoft.in>
2026-07-09 13:02:08 +0530

    Update auth secret

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -50,6 +50,6 @@ export const authOptions: AuthOptions = {
             return session;
         },
     },
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET ?? process.env.AUTH_SECRET,
 };
 
